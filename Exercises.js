989ec581ad4bba6fd4e2db5e3efeb7361c74f376
adishimo <77383584+adishimo@users.noreply.github.com>
2021-01-22 16:40:02 +0200

    Questions 2-6

--- Exercises.js
+++ Exercises.js
@@ -1,60 +1,94 @@
 //Question 1
 function myReverse(str) {
-  var newString = "";
-  for (var i = str.length - 1; i >= 0; i--) {
+  let newString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
       newString += str[i];
   }
   return newString; 
 }
-myReverse("adi");
+console.log(myReverse("adi"));
 
 //Question 2
-function allCombinations() {
-  var buff = [];
-var res = [];
-for (i=0;i<s.length;i++){
-    buff = [s[i]];
-    var index=0;
-    while(res[index]){
-        buff.push(''+res[index]+s[i]);
-        index++;
+function allCombinations(str) {
+  let combinations = [];
+  let i = 0;
+  let lenStr = str.length;
+  while (i < lenStr) {
+    let j = str[i];
+    let x;
+    let arrTemp = [j];
+    for (x in combinations) {
+      arrTemp.push("" + combinations[x] + j);
     }
-    res = res.concat(buff);
+    combinations = combinations.concat(arrTemp);
+    i++;
+  }
+  return combinations;
 }
-return res;
-}
- 
+console.log(allCombinations("dog"));
 
 //Question 3
-function allCaps(str) {
-  let wordsArray = str.toLowerCase().split(' ')
+function allCaps(str) { // str = "a very long daY"
+  let wordsArray = str.toLowerCase().split(' ') // ["a", "very", "long", "day"]
   let capsArray = []
+  function blabla(word){
+    capsArray.push(word[0].toUpperCase() + word.slice(1)) //word[0] = v, V, word.slice(1) = ery, Very
 
-  wordsArray.forEach(word => {
-      capsArray.push(word[0].toUpperCase() + word.slice(1))
-  });
+  }
+  wordsArray.forEach(blabla);
 
-  return capsArray.join(' ')
+  return capsArray.join(' ') // "A Very Long Day"
 }
 
 //Question 4
 function myPower(x, n) {
-  let num = Math.pow(base, exponent);
-  return num;
+  let ans = 1;
+  for (let i = 1; i <= n; i++) {
+    ans *= x;
+  }
+  return ans;
 }
-myPower(2,3)
+console.log(myPower(7, 3));
 
 
 //Question 5
 function getFirstNotRepeating(str) {
-  // your code here
-  return "";
+  let arr = str.split("");
+  let result = "";
+  let ctr = 0; 
+  for (let i = 0; i < arr.length; i++) {
+    ctr = 0;
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        ctr += 1;
+      }
+    }
+    if (ctr < 2) {
+      result = arr[i];
+      break;
+    }
+  }
+  return result;
 }
+console.log(getFirstNotRepeating("abacddbec"));
+
 
 //Question 6 (Bonus)
 function isPrefectNumber(num) {
-  // your code here
-  return "I'm not interested in the bonus question :(";
+  function isPrefectNumber(num) {
+    let temp = 0;
+    for(let i=1; i<=num/2; i++){
+      if(num%i === 0){
+        temp += i;
+      }
+    }
+    if(temp === num && temp !== 0){
+      console.log("It is a perfect number.");
+    } else {
+      console.log("It is not a perfect number.");
+    }
+  }
+  isPrefectNumber(28);
 }
 
 // *** Playground ***
