d75e244f1538ea0d6da45344571ac934031f3a18
adishimo <77383584+adishimo@users.noreply.github.com>
2021-01-22 13:53:44 +0200

    q1

--- Exercises.js
+++ Exercises.js
@@ -1,29 +1,49 @@
 //Question 1
 function myReverse(str) {
-  // your code here
-  return "";
+  var newString = "";
+  for (var i = str.length - 1; i >= 0; i--) {
+      newString += str[i];
+  }
+  return newString; 
 }
+myReverse("adi");
 
 //Question 2
-function allCombinations(str) {
-  let combinations = [];
-  // your code here
-
-  //
-  return combinations;
+function allCombinations() {
+  var buff = [];
+var res = [];
+for (i=0;i<s.length;i++){
+    buff = [s[i]];
+    var index=0;
+    while(res[index]){
+        buff.push(''+res[index]+s[i]);
+        index++;
+    }
+    res = res.concat(buff);
 }
+return res;
+}
+ 
 
 //Question 3
 function allCaps(str) {
-  // your code here
-  return "";
+  let wordsArray = str.toLowerCase().split(' ')
+  let capsArray = []
+
+  wordsArray.forEach(word => {
+      capsArray.push(word[0].toUpperCase() + word.slice(1))
+  });
+
+  return capsArray.join(' ')
 }
 
 //Question 4
 function myPower(x, n) {
-  // your code here
-  return "";
+  let num = Math.pow(base, exponent);
+  return num;
 }
+myPower(2,3)
+
 
 //Question 5
 function getFirstNotRepeating(str) {
@@ -39,7 +59,7 @@ function isPrefectNumber(num) {
 
 // *** Playground ***
 // Feel free to run and test your code here on your own
-
+ 
 // *** End of Playground ***
 
 // Don't touch me :)
